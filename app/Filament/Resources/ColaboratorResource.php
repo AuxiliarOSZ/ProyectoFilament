e05603d1bdfa120b310ab156e7561caa5e7c7dfe
aprendizOSZ <programacionaprendiz@gmail.com>
2025-05-23 06:58:40 -0600

    feat: crear formulario y tabla Colaboradores; fix: crear migración cambiando tipo de campo de mobile y phone a string

--- app/Filament/Resources/ColaboratorResource.php
+++ app/Filament/Resources/ColaboratorResource.php
@@ -6,6 +6,14 @@ use App\Filament\Resources\ColaboratorResource\Pages;
 use App\Filament\Resources\ColaboratorResource\RelationManagers;
 use App\Models\Colaborator;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -21,134 +29,186 @@ class ColaboratorResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form    
+        return $form  
+        
             ->schema([
-                Forms\Components\Grid::make([
-                    'default' => 1,
-                    'sm' => 2,
-                    'md' => 2,
-                    'lg' => 3,
-                ])
-                ->schema([
-                    Forms\Components\Select::make('document_type')
-                        ->label('Tipo de documento')
-                        ->options([
-                            'CC' => 'Cédula de ciudadanía',
-                            'CE' => 'Cédula de extranjería',
-                            'TI' => 'Tarjeta de identidad',
-                            //'PA' => 'Pasaporte',
-                        ])
-                        ->required()
-                        ->reactive(),
+                Section::make('Datos personales')
+                
+                    ->schema([
+                        // Seccion: Datos personales de la persona
+                        Forms\Components\Grid::make()
+                            ->schema([
+                                Forms\Components\Section::make()
+                                    ->schema([
+                                        Grid::make(1)
+                                            ->columns([
+                                                'default' => 1,
+                                                'sm' => 2,
+                                                'md' => 4,
+                                                'lg' => 4,
+                                            ])
+                                            ->schema([
+                                                Select::make('document_type')
+                                                    ->label('Tipo de documento')
+                                                    ->options([
+                                                        'CC' => 'Cédula de ciudadanía',
+                                                        'CE' => 'Cédula de extranjería',
+                                                        'TI' => 'Tarjeta de identidad',
+                                                    ])
+                                                    ->required()
+                                                    ->columnSpan(2)
+                                                    ->reactive(),
 
-                    Forms\Components\TextInput::make('document_number')
-                        ->label('Número de documento')
-                        /*->rules(function(callable $get){
-                            return $get('document_type') === 'PA'
-                            ? ['required', 'alpha_num']
-                            : ['required', 'numeric'];
-                        }) */
-                        ->required()
-                        ->maxLength(10)
-                        ->validationMessages([
-                            'required' => 'El número de documento es obligatorio.',
-                            'alpha_num' => 'El número de documento debe contener solo números y letras.',
-                            'numeric' => 'El número de documento debe ser numérico.',
-                        ]),
+                                                TextInput::make('document_number')
+                                                    ->label('Número de documento')
+                                                    ->required()
+                                                    ->columnSpan(2)
+                                                    ->maxLength(10)
+                                                    ->validationMessages([
+                                                        'required' => 'El número de documento es obligatorio.',
+                                                        'alpha_num' => 'El número de documento debe contener solo números y letras.',
+                                                        'numeric' => 'El número de documento debe ser numérico.',
+                                                    ]),
 
-                    Forms\Components\TextInput::make('first_name')
-                        ->label('Nombres completos')
-                        ->required()
-                        ->maxLength(255),
+                                                TextInput::make('first_name')
+                                                    ->label('Nombres completos')
+                                                    ->required()
+                                                    ->columnSpan(2)
+                                                    ->maxLength(255),
 
-                    Forms\Components\TextInput::make('last_name')
-                        ->label('Apellidos completos')
-                        ->required()
-                        ->maxLength(255),
+                                                TextInput::make('last_name')
+                                                    ->label('Apellidos completos')
+                                                    ->required()
+                                                    ->columnSpan(2)
+                                                    ->maxLength(255),
 
-                    Forms\Components\Select::make('gender')
-                        ->label('Género')
-                        ->options([
-                            'M' => 'Masculino',
-                            'F' => 'Femenino',
-                            'O' => 'Otro',
-                        ])
-                        ->required(),
+                                                Select::make('gender')
+                                                    ->label('Género')
+                                                    ->options([
+                                                        'M' => 'Masculino',
+                                                        'F' => 'Femenino',
+                                                        'O' => 'Otro',
+                                                    ])
+                                                    ->required(),
 
-                    Forms\Components\DatePicker::make('birth_date')
-                        ->label('Fecha de nacimiento')
-                        ->required(),
+                                                    
+                                                DatePicker::make('birth_date')
+                                                    ->label('Fecha de nacimiento')
+                                                    ->required(),
 
-                    Forms\Components\TextInput::make('personal_email')
-                        ->label('Correo electrónico personal')
-                        ->email()
-                        ->required()
-                        ->maxLength(255),
+                                                TextInput::make('personal_email')
+                                                    ->label('Correo electrónico personal')
+                                                    ->email()
+                                                    ->required()
+                                                    ->columnSpan(2)
+                                                    ->maxLength(255),
 
-                    Forms\Components\TextInput::make('corporate_email')
-                        ->label('Correo electrónico corporativo')
-                        ->email()
-                        ->maxLength(255),
 
-                    Forms\Components\TextInput::make('mobile')
-                        ->label('Número de teléfono móvil')
-                        ->maxLength(20)
-                        ->required(),
+                                                TextInput::make('mobile')
+                                                    ->label('Número de teléfono móvil')
+                                                    ->numeric()
+                                                    ->maxLength(20)
+                                                    ->required(),
 
-                    Forms\Components\TextInput::make('phone')
-                        ->label('Número de teléfono fijo')
-                        ->maxLength(20),
+                                                TextInput::make('phone')
+                                                    ->label('Número de teléfono fijo')
+                                                    ->numeric()
+                                                    ->maxLength(20),
+                                                
+                                                TextInput::make('address')
+                                                    ->label('Dirección')
+                                                    ->required()
+                                                    ->maxLength(255),
+
+                                                TextInput::make('residential_city')
+                                                    ->label('Ciudad de residencia')
+                                                    ->required()
+                                                    ->maxLength(255),
+
+                                                Select::make('education_level')
+                                                    ->label('Nivel educativo')
+                                                    ->options([
+                                                        'Bachiller' => 'Bachiller',
+                                                        'Tecnico' => 'Tecnico',
+                                                        'Tecnologo' => 'Tecnologo',
+                                                        'Profesional' => 'Profesional',
+                                                    ])
+                                                    ->required(),
+
+                                                Forms\Components\Placeholder::make('')
+                                                ->extraAttributes(['class' => 'text-sm text-gray-500 italic'])
+                                                    ->content('Recuerda llenar todos los campos obligatorios marcados con * antes de crear el colaborador.')
+                                                    ->columnSpan(3)
+
+
+                                            ]),
+                                    ])
+                                    ->columnSpan(['lg' => 3]),
+
+                                // Documentos
+                                Forms\Components\Section::make('Documentos')
+                                    ->schema([
+                                        FileUpload::make('eps')
+                                            ->label('EPS')
+                                            ->required()
+                                            ->extraAttributes(['class' => 'min-h-[42px] py-8 w-[250px]']),
+
+                                        FileUpload::make('arl')
+                                            ->label('ARL')
+                                            ->required()
+                                            ->extraAttributes(['class' => 'min-h-[42px] py-8 w-[250px]']),
+                                    ])
+                                    ->columnSpan(['lg' => 1]),
+                            ])
+                            ->columns(4),
+
+                            // Seccion: Datos laborales de la persona
+                            Section::make('Datos de laborales')
+                                ->schema([
+                                    Grid::make(1)
+                                            ->columns([
+                                                'default' => 1,
+                                                'sm' => 2,
+                                                'md' => 2,
+                                                'lg' => 2,
+                                            ])
+                                            ->schema([
+                                    
+
+                                                TextInput::make('corporate_email')
+                                                    ->label('Correo electrónico corporativo')
+                                                    ->email()
+                                                    ->maxLength(255),
+                                                
+                                                DatePicker::make('hire_date')
+                                                    ->label('Fecha de contratación')
+                                                    ->required(),
+
+                                                Select::make('job_position')
+                                                    ->label('Cargo')
+                                                    ->options([
+                                                        'Jefe de proyecto' => 'Jefe de proyecto',
+                                                        'Desarrollador' => 'Desarrollador',
+                                                        'Analista' => 'Analista',
+                                                        'Tester' => 'Tester',
+                                                    ])
+                                                    ->columnSpan(2)
+                                                    ->required(),
+
+                                                Textarea::make('notes')
+                                                    ->label('Observaciones')
+                                                    ->columnSpan('full')
+                                                    ->required(),
+
+                                                Toggle::make('status')
+                                                    ->label('Activo')
+                                                    ->required()
+                                            ])
                     
-                    Forms\Components\TextInput::make('address')
-                        ->label('Dirección')
-                        ->required()
-                        ->maxLength(255),
-
-                    Forms\Components\TextInput::make('residential_city')
-                        ->label('Ciudad de residencia')
-                        ->required()
-                        ->maxLength(255),
-
-                    Forms\Components\Select::make('education_level')
-                        ->label('Nivel educativo')
-                        ->options([
-                            'Bachiller' => 'Bachiller',
-                            'Tecnico' => 'Tecnico',
-                            'Tecnologo' => 'Tecnologo',
-                            'Profesional' => 'Profesional',
-                        ])
-                        ->required(),
-
-                    Forms\Components\Select::make('job_position')
-                        ->label('Cargo')
-                        ->options([
-                            'Jefe de proyecto' => 'Jefe de proyecto',
-                            'Desarrollador' => 'Desarrollador',
-                            'Analista' => 'Analista',
-                            'Tester' => 'Tester',
-                        ])
-                        ->required(),
-
-                    Forms\Components\DatePicker::make('hire_date')
-                        ->label('Fecha de contratación')
-                        ->required(),
-                    Forms\Components\FileUpload::make('eps')
-                        ->label('EPS')
-                        ->required(),
-
-                    Forms\Components\FileUpload::make('arl')
-                        ->label('ARL')
-                        ->required(),
-
-                    Forms\Components\Textarea::make('notes')
-                        ->label('Observaciones')
-                        ->required(),
-
-                    Forms\Components\Toggle::make('status')
-                        ->label('Activo')
-                        ->required(),
-                ])
-            ]);
+            
+                                        ])
+                                    ])
+                        ]);
     }
 
     public static function table(Table $table): Table
@@ -232,6 +292,7 @@ class ColaboratorResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
