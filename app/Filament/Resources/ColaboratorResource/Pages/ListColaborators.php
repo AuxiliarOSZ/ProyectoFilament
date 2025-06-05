132b722e970da91313c8cf9ec08050029f095b21
AuxiliarOSZ <auxiliarprogramacion0@gmail.com>
2025-06-05 14:21:10 -0500

    feat(importación): agregar descarga de plantilla Excel para colaboradores
    Se añadió un botón en la interfaz de importación que permite descargar
    una plantilla en formato Excel con las columnas necesarias para el registro
    masivo de colaboradores.

--- app/Filament/Resources/ColaboratorResource/Pages/ListColaborators.php
+++ app/Filament/Resources/ColaboratorResource/Pages/ListColaborators.php
@@ -3,22 +3,46 @@
 namespace App\Filament\Resources\ColaboratorResource\Pages;
 
 use App\Filament\Resources\ColaboratorResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Actions\CreateAction;
 use App\Imports\ColaboratorsImport;
+use Filament\Forms\Components\Actions\Action;
 
-
+/**
+ * Clase ListColaborators
+ *
+ * Página para listar los colaboradores en el recurso ColaboratorResource.
+ * Permite importar colaboradores desde un archivo Excel y crear nuevos registros.
+ *
+ * @package App\Filament\Resources\ColaboratorResource\Pages
+ */
 class ListColaborators extends ListRecords
 {
+    /**
+     * El recurso asociado a la página.
+     *
+     * @var string
+     */
     protected static string $resource = ColaboratorResource::class;
 
+    /**
+     * Obtiene las acciones del encabezado de la página.
+     *
+     * @return array Acciones disponibles en el encabezado, incluyendo importación desde Excel y creación de registros.
+     */
     protected function getHeaderActions(): array
     {
         return [
             \EightyNine\ExcelImport\ExcelImportAction::make()
                 ->color('secondary')
-                ->use(ColaboratorsImport::class),
+                ->use(ColaboratorsImport::class)
+                ->sampleFileExcel(
+                    url: asset('data/excel/colaborators-template.xlsx'),
+                    sampleButtonLabel: 'Descargar plantilla',
+                    customiseActionUsing: fn(Action $action) => $action->color('secondary')
+                        ->icon('heroicon-m-clipboard')
+                        ->requiresConfirmation(),
+                ),
             CreateAction::make(),
         ];
     }
